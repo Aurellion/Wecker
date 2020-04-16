ce30039e3f53806a8aca5ef151163ad7b654485c
Johannes B <johannes.bock@live.de>
2020-04-16 12:31:57 +0200

    Projektdateien hinzufügen.

---
+++ Wecker/Appointment.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wecker
+{
+    class Appointment
+    {
+        private int minutes { get; set; }
+        private int hours { get; set; }
+        private string title { get; set; }
+
+        public Appointment(string title, int hours, int minutes)
+        {
+            this.title = title;
+            this.hours = hours;
+            this.minutes = minutes;
+        }
+
+        public override string ToString()
+        {
+            return hours.ToString("D2") + ":" + minutes.ToString("00") + " " +title;
+        }
+
+        public virtual void Alarm()
+        {
+
+        }
+
+        public bool IsActive()
+        {
+            DateTime d = DateTime.Now;
+            if (d.Hour == hours && d.Minute == minutes) return true;
+            else return false;
+        }
+
+    }
+
+    class AppointmentSound : Appointment
+    {
+        public AppointmentSound(string titel, int hours, int minutes ) : base(titel, hours, minutes)
+        {            
+        }
+
+        public override string ToString()
+        {
+            return "K "+base.ToString();
+        }
+
+        public override void Alarm()
+        {
+            System.Media.SystemSounds.Beep.Play();
+        }
+    }
+
+    class AppointmentColour : Appointment
+    {
+        public AppointmentColour(string titel, int hours, int minutes) : base(titel, hours, minutes)
+        {
+        }
+
+        public override string ToString()
+        {
+            return "F " + base.ToString();
+        }
+
+        public override void Alarm()
+        {
+            //hier ergaenzen
+        }
+    }
+}
---
+++ Wecker/MainWindow.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wecker
+{
+    /// <summary>
+    /// Interaktionslogik für MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        //timer
+        System.Windows.Threading.DispatcherTimer timer = new System.Windows.Threading.DispatcherTimer();
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            timer.Interval = TimeSpan.FromMinutes(1);
+            timer.Start();
+            timer.Tick += checkAlarms;
+        }
+
+        void checkAlarms(object sender, EventArgs e)
+        {
+            for (int i = 0; i < LB_Appointments.Items.Count; i++)
+            {
+                Appointment t = (Appointment)LB_Appointments.Items[i];
+                if(t.IsActive())
+                {
+                    t.Alarm();
+                }
+            }
+        }
+
+        private void Button_TerminHinzufügen_Click(object sender, RoutedEventArgs e)
+        {
+            string titel = TB_Title.Text;
+            int minuten, stunden;
+            bool result;
+
+            result = int.TryParse(TB_Hours.Text, out stunden);
+            result &= int.TryParse(TB_Minutes.Text, out minuten);
+
+            if(result && stunden >= 0 && stunden <= 23 && minuten >=0 && minuten <=59)
+            {
+                if (RB_Colour.IsChecked == true)
+                {
+                    Appointment Termin = new AppointmentColour(titel, stunden, minuten);
+                    LB_Appointments.Items.Add(Termin);
+                  
+                }
+                else
+                {
+                    Appointment Termin = new AppointmentSound(titel, stunden, minuten);
+                    LB_Appointments.Items.Add(Termin);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Fehler");
+            }
+        }
+
+
+        private void Button_TerminLoeschen_Click(object sender, RoutedEventArgs e)
+        {
+            int i = LB_Appointments.SelectedIndex;
+            if (i >= 0)
+            {
+                LB_Appointments.Items.RemoveAt(i);
+            }
+        }
+    }
+
+}
